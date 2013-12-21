243d128274523b6d2d4ef5355039b8c79da5253b
Spencer Russell <spencer.f.russell@gmail.com>
2013-12-20 20:35:51 -0500

    now unwrap works with multidimensional arrays

--- src/util.jl
+++ src/util.jl
@@ -2,19 +2,30 @@ module Util
 
 export unwrap!, unwrap
 
-function unwrap!(p)
-    if length(p) < 2
-        return p
+function unwrap!(m::Array, dim::Integer=ndims(m); range::Number=2pi)
+    if size(m, dim) < 2
+        return m
     end
-    for i = 2:length(p)
-        d = p[i] - p[i-1]
-        if abs(d) > pi
-            p[i] -= floor((d+pi) / (2pi)) * 2pi
-        end
+    for i = 2:size(m, dim)
+        d = slicedim(m, dim, i) - slicedim(m, dim, i-1)
+        slice_tuple = ntuple(ndims(m), n->(n==dim ? (i:i) : (1:size(m,n))))
+        offset = floor((d+pi) / (2pi)) * 2pi
+#        println("offset: ", offset)
+#        println("typeof(offset): ", typeof(offset))
+#        println("typeof(m[slice_tuple...]): ", typeof(m[slice_tuple...]))
+#        println("slice_tuple: ", slice_tuple)
+#        println("m[slice_tuple...]: ", m[slice_tuple...])
+        m[slice_tuple...] = m[slice_tuple...] - offset
     end
-    return p
+    return m
 end
 
-unwrap(p) = unwrap!(copy(p))
+function unwrap(m::Array, args...; kwargs...)
+    unwrap!(copy(m), args...; kwargs...)
+end
+
+#function unwrap(m::Array; args...)
+#    unwrap!(copy(m); args...)
+#end
 
 end # end module definition
--- test/util.jl
+++ test/util.jl
@@ -13,3 +13,11 @@ res_v = unwrap(test_v)
 @test_approx_eq(test_v, [0.1, 0.2, 0.3 + 2pi, 0.4])
 unwrap!(test_v)
 @test_approx_eq(test_v, [0.1, 0.2, 0.3, 0.4])
+
+# test multi-dimensional unwrapping
+wrapped = [0.1, 0.2 + 2pi, 0.3, 0.4]
+unwrapped = [0.1, 0.2, 0.3, 0.4]
+wrapped = hcat(wrapped, wrapped)
+unwrapped = hcat(unwrapped, unwrapped)
+@test_approx_eq(unwrap(wrapped), wrapped)
+@test_approx_eq(unwrap(wrapped, 1), unwrapped)
