5b95fa0d2feef924c8b2a9014fed60088a5f1181
Simon Kornblith <simon@simonster.com>
2014-04-11 15:42:52 -0400

    Use Reexport.jl

--- src/DSP.jl
+++ src/DSP.jl
@@ -6,20 +6,6 @@ include("periodogram.jl")
 include("fftfilt.jl")
 include("filter_design.jl")
 
-using DSP.Windows, DSP.Periodogram, DSP.FFTFilt, DSP.FilterDesign, DSP.Util
-
-export
-       # Util
-       unwrap!, unwrap, hilbert,
-       # Windows
-       rect, hanning, hamming, tukey, cosine, lanczos,
-       triang, bartlett, gaussian, bartlett_hann, blackman,
-       kaiser, dpss,
-       # Periodogram
-       arraysplit, periodogram, welch_pgram, bartlett_pgram,
-       # FFTFilt
-       fftfilt, firfilt,
-       # FilterDesign
-       ZPKFilter, TFFilter, BiquadFilter, SOSFilter, Butterworth, Lowpass,
-       Highpass, Bandpass, Bandstop, analogfilter, digitalfilter
+using Reexport
+@reexport using .Windows, .Periodogram, .FFTFilt, .FilterDesign, .Util
 end
