a71a055ba18e5ddd2a9ed7bc01554b762947aaf7
Simon Kornblith <simon@simonster.com>
2014-06-20 14:35:05 -0400

    Rename periodogram.jl -> periodograms.jl in accordance with module

--- src/DSP.jl
+++ src/DSP.jl
@@ -2,7 +2,7 @@ module DSP
 
 include("util.jl")
 include("windows.jl")
-include("periodogram.jl")
+include("periodograms.jl")
 include("fftfilt.jl")
 include("filter_design.jl")
 include("filter_response.jl")
