4865ded13bec8235158aca97ca0058632645f313
Jay Kickliter <jay@kickliter.com>
2015-05-24 15:36:35 -0600

    Removed unnecessary usage of `Float64` so test can run on v0.3

--- test/resample.jl
+++ test/resample.jl
@@ -52,14 +52,14 @@ y4_jl = resample(x_ml, rate, h4_ml)
 ratio = 1//2
 h     = resample_filter(ratio)
 r0    = abs(freqz(PolynomialRatio(h, [1]), 0))
-rc    = abs(freqz(PolynomialRatio(h, [1]), Float64(ratio)*pi))
+rc    = abs(freqz(PolynomialRatio(h, [1]), ratio*π))
 @test isapprox(r0, 1.0)
 @test isapprox(rc, num(ratio)/2, rtol=0.001)
 
 ratio = 1//32
 h     = resample_filter(ratio)
 r0    = abs(freqz(PolynomialRatio(h, [1]), 0))
-rc    = abs(freqz(PolynomialRatio(h, [1]), Float64(ratio)*pi))
+rc    = abs(freqz(PolynomialRatio(h, [1]), ratio*π))
 @test isapprox(r0, num(ratio))
 @test isapprox(rc, num(ratio)/2, rtol=0.001)
 
@@ -68,23 +68,23 @@ rc    = abs(freqz(PolynomialRatio(h, [1]), Float64(ratio)*pi))
 ratio = 2//1
 h     = resample_filter(ratio)
 r0    = abs(freqz(PolynomialRatio(h, [1]), 0))
-rc    = abs(freqz(PolynomialRatio(h, [1]), Float64(1/ratio)*pi))
+rc    = abs(freqz(PolynomialRatio(h, [1]), 1/ratio*π))
 @test isapprox(r0, num(ratio))
 @test isapprox(rc, num(ratio)/2, rtol=0.001)
 
 ratio = 32//1
 h     = resample_filter(ratio)
 r0    = abs(freqz(PolynomialRatio(h, [1]), 0))
-rc    = abs(freqz(PolynomialRatio(h, [1]), Float64(1/ratio)*pi))
+rc    = abs(freqz(PolynomialRatio(h, [1]), 1/ratio*π))
 @test isapprox(r0, num(ratio))
 @test isapprox(rc, num(ratio)/2, rtol=0.001)
 
 # Arbitrary rate resampling
-ratio = Float64(π)
+ratio = 3.141592653589793
 Nϕ    = 32
 fc    = 1/Nϕ
 h     = resample_filter(ratio, Nϕ)
 r0    = abs(freqz(PolynomialRatio(h, [1]), 0))
-rc    = abs(freqz(PolynomialRatio(h, [1]), Float64(fc)*pi))
+rc    = abs(freqz(PolynomialRatio(h, [1]), fc*π))
 @test isapprox(r0, Nϕ)
 @test isapprox(rc, Nϕ/2, rtol=0.001)
