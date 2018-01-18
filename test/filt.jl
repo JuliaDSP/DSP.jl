2a804b3cf51260aae57264c3ff767cfb23032d97
Martin Holters <martin.holters@hsu-hh.de>
2018-01-18 09:39:19 +0100

    Add `using Compat.Random` to tests for `srand`

--- test/filt.jl
+++ test/filt.jl
@@ -1,5 +1,5 @@
 !(dirname(@__FILE__) in LOAD_PATH) && push!(LOAD_PATH, dirname(@__FILE__))
-using DSP, Compat, Compat.Test, FilterTestHelpers
+using DSP, Compat, Compat.Test, Compat.Random, FilterTestHelpers
 
 #
 # filt with different filter forms
