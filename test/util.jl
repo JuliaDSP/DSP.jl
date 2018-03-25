713ad24c34a0c4908bd949d3f37e4fa409b3240f
Tom Krauss <tom@epiqsolutions.com>
2018-03-24 20:10:41 -0500

    add test for esprit

--- test/util.jl
+++ test/util.jl
@@ -104,6 +104,24 @@ end
 @test nextfastfft((64,65,127)) == (64,70,128)
 @test nextfastfft(64,65,127) == nextfastfft((64,65,127))
 
+# esprit
+# create a sum of sinusoids in noise, and estimate their frequencies
+Fs = 10000.0           # sampling frequency in Hz
+duration = 1           # length of signal, in seconds
+n = Int(Fs * duration) # number of samples
+t = collect((1:n)/Fs)  # time vector
+frequencies = [1000.0 1250.0]
+amplitudes = [2.0 1.5]
+phases = [0.7 -1.0]
+x = exp.( 1im*2*π*t*frequencies .+ phases)
+noise = randn(n, 2)*[1;1im]
+sigma = 0.1
+noise *= sigma
+x += noise
+M = 300
+p = 2                  # number of sinusoids to estimate
+frequencies_estimated = esprit(x, M, p, Fs)
+@test frequencies ≈ frequencies_estimated
 
 ## COMMON DSP TOOLS
 
