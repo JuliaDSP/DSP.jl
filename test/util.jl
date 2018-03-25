f4f29adfb7fe0d6bd56e8110951a2d9f743ef64a
Tom Krauss <tom@epiqsolutions.com>
2018-03-24 20:20:21 -0500

    fix bugs in esprit test

--- test/util.jl
+++ test/util.jl
@@ -113,7 +113,7 @@ t = collect((1:n)/Fs)  # time vector
 frequencies = [1000.0 1250.0]
 amplitudes = [2.0 1.5]
 phases = [0.7 -1.0]
-x = exp.( 1im*2*π*t*frequencies .+ phases)
+x = exp.( 1im*2*π*t*frequencies .+ phases) * amplitudes'
 noise = randn(n, 2)*[1;1im]
 sigma = 0.1
 noise *= sigma
@@ -121,7 +121,7 @@ x += noise
 M = 300
 p = 2                  # number of sinusoids to estimate
 frequencies_estimated = esprit(x, M, p, Fs)
-@test frequencies ≈ frequencies_estimated
+@test frequencies' ≈ frequencies_estimated
 
 ## COMMON DSP TOOLS
 
