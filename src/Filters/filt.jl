654beaf0cec96acc252576e7c44c1197d5d9a009
Galen Lynch <galen.lynch@gmail.com>
2018-07-03 16:46:30 -0400

    Make fftfilt! use output array passed as argument
    It was ignoring the output array passed as an argument and creating a new array
    instead.

--- src/Filters/filt.jl
+++ src/Filters/filt.jl
@@ -531,7 +531,6 @@ function _fftfilt!(out::AbstractArray{T}, b::AbstractVector{T}, x::AbstractArray
     L = min(nx, nfft - (nb - 1))
     tmp1 = Vector{T}(undef, nfft)
     tmp2 = Vector{Complex{T}}(undef, nfft >> 1 + 1)
-    out = Array{T}(undef, size(x))
 
     p1 = plan_rfft(tmp1)
     p2 = plan_brfft(tmp2, nfft)
@@ -539,7 +538,7 @@ function _fftfilt!(out::AbstractArray{T}, b::AbstractVector{T}, x::AbstractArray
     # FFT of filter
     filterft = similar(tmp2)
     copyto!(tmp1, b)
-    tmp1[nb+1:end] .= zero(T)
+    tmp1[nb+1:end] = zero(T)
     mul!(filterft, p1, tmp1)
 
     # FFT of chunks
@@ -550,8 +549,8 @@ function _fftfilt!(out::AbstractArray{T}, b::AbstractVector{T}, x::AbstractArray
             xstart = off - nb + npadbefore + 1
             n = min(nfft - npadbefore, nx - xstart + 1)
 
-            tmp1[1:npadbefore] .= zero(T)
-            tmp1[npadbefore+n+1:end] .= zero(T)
+            tmp1[1:npadbefore] = zero(T)
+            tmp1[npadbefore+n+1:end] = zero(T)
 
             copyto!(tmp1, npadbefore+1, x, colstart+xstart, n)
             mul!(tmp2, p1, tmp1)
