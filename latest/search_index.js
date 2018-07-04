084b0c0d3e49a8437913486c48c79d896f96e496
autodocs <autodocs>
2018-07-04 06:37:03 +0000

    build based on 6e8901b

--- latest/search_index.js
+++ latest/search_index.js
@@ -617,19 +617,19 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "util.html#DSP.Util.unwrap",
+    "location": "util.html#DSP.Unwrap.unwrap",
     "page": "Util - utility functions",
-    "title": "DSP.Util.unwrap",
+    "title": "DSP.Unwrap.unwrap",
     "category": "function",
-    "text": "unwrap(m; dims=nothing, range=2pi)\n\nAssumes m (along the single dimension dims) to be sequences of values that have been wrapped to be inside the given range (centered around zero), and undoes the wrapping by identifying discontinuities.\n\nA common usage is for a phase measurement over time, such as when comparing successive frames of a short-time-fourier-transform, as each frame is wrapped to stay within (-pi, pi].\n\n\n\n"
+    "text": "unwrap(m; kwargs...)\n\nAssumes m to be a sequence of values that has been wrapped to be inside the given range (centered around zero), and undoes the wrapping by identifying discontinuities. If a single dimension is passed to dims, then m is assumed to have wrapping discontinuities only along that dimension. If a range of dimensions, as in 1:ndims(m), is passed to dims, then m is assumed to have wrapping discontinuities across all ndims(m) dimensions.\n\nA common usage for unwrapping across a singleton dimension is for a phase measurement over time, such as when comparing successive frames of a short-time-fourier-transform, as each frame is wrapped to stay within (-pi, pi].\n\nA common usage for unwrapping across multiple dimensions is for a phase measurement of a scene, such as when retrieving the phase information of of an image, as each pixel is wrapped to stay within (-pi, pi].\n\nArguments\n\nm::AbstractArray{T, N}: Array to unwrap.\ndims=nothing: Dimensions along which to unwrap. If dims is an integer, then   unwrap is called on that dimension. If dims=1:ndims(m), then m is unwrapped   across all dimensions.\nrange=2pi: Range of wrapped array.\ncircular_dims=(false, ...):  When an element of this tuple is true, the   unwrapping process will consider the edges along the corresponding axis   of the array to be connected.\nrng=GLOBAL_RNG: Unwrapping of arrays with dimension > 1 uses a random   initialization. A user can pass their own RNG through this argument.\n\n\n\n"
 },
 
 {
-    "location": "util.html#DSP.Util.unwrap!",
+    "location": "util.html#DSP.Unwrap.unwrap!",
     "page": "Util - utility functions",
-    "title": "DSP.Util.unwrap!",
+    "title": "DSP.Unwrap.unwrap!",
     "category": "function",
-    "text": "unwrap!(m; dims=nothing, range=2pi)\n\nIn-place version of unwrap.\n\n\n\nunwrap!(y, m; dims=nothing, range=2pi)\n\nUnwrap m storing the result in y, see unwrap.\n\n\n\n"
+    "text": "unwrap!(m; kwargs...)\n\nIn-place version of unwrap.\n\n\n\nunwrap!(y, m; kwargs...)\n\nUnwrap m storing the result in y, see unwrap.\n\n\n\n"
 },
 
 {
