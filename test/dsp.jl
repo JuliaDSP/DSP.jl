8cb50824f56d9bf9f22a58c65466b7425dd448fc
hdictus <h.t.dictus@gmail.com>
2019-03-21 15:39:51 +0100

    Implement basic ND-convolution
    breaks xcorr @inferred
    breaks conv between int and float

--- test/dsp.jl
+++ test/dsp.jl
@@ -1,7 +1,7 @@
 # This file was formerly a part of Julia. License is MIT: https://julialang.org/license
 
 using Compat, Compat.Test, DSP
-import DSP: filt, filt!, deconv, conv, conv2, xcorr
+import DSP: filt, filt!, deconv, conv, conv, xcorr
 
 
 @testset "filt" begin
@@ -31,6 +31,115 @@ import DSP: filt, filt!, deconv, conv, conv2, xcorr
     @test_throws ArgumentError filt!([1, 2], [1], [1], [1])
 end
 
+
+@testset "conv" begin
+        
+    @testset "conv-1D" begin
+        # Convolution
+        a = [1, 2, 1, 2]
+        b = [1, 2, 3]
+        expectation = [1, 4, 8, 10, 7, 6]
+        im_expectation = [1, 3, 6, 6, 5, 3]
+        @test conv(a, b) == expectation
+        @test conv(complex.(a, 1), complex(b)) == complex.(expectation,
+                                                           im_expectation)
+        fa = convert(Array{Float64}, a)
+        fb = convert(Array{Float64}, b)
+        fexp = convert(Array{Float64}, expectation)
+        im_fexp = convert(Array{Float64}, im_expectation)
+        @test conv(fa, fb) ≈ fexp
+        @test conv(complex.(fa, 1.), complex(fb)) ≈ complex.(fexp, im_fexp)
+        
+        @test_broken conv(fa, b) ≈ fexp
+        @test_broken conv(fb, a) ≈ fexp
+        @test_broken conv(complex(fa, 1.), complex(b)) ≈ complex.(fexp, im_fexp)
+    end
+
+
+    @testset "conv-2D" begin
+        a =[1 2 1;
+            2 3 1;
+            1 2 1]  
+        
+        b = [3 2;
+             0 1]
+        
+        expectation = [3 8 7 2;
+                       6 14 11 3;
+                       3 10 10 3;
+                       0 1 2 1]
+        im_expectation = [3 5 5 2;
+                          3 6 6 3;
+                          3 6 6 3;
+                          0 1 1 1]
+
+        # Integers
+        # Real Integers
+        @test conv(a, b) == expectation
+        # Complex
+        @test conv(complex.(a, 1), complex.(b)) == complex.(expectation,
+                                                             im_expectation)
+        # Floats
+        fa = convert(Array{Float64}, a)
+        fb = convert(Array{Float64}, b)
+        fexp = convert(Array{Float64}, expectation)
+        im_fexp = convert(Array{Float64}, im_expectation)
+        # Real
+        @test conv(fa, fb) == fexp
+        # Complex
+        @test conv(complex.(fa, 1), complex.(fb)) == complex.(fexp,
+                                                               im_fexp)
+        @test_broken conv(fa, b) ≈ fexp
+        @test_broken conv(fb, a) ≈ fexp
+        @test_broken conv(complex(fa, 1.), complex(b)) ≈ complex.(fexp, im_fexp)
+    end
+
+    @testset "conv-ND" begin
+        # is it safe to assume that if conv works for
+        # int/float/complex in 1 and 2 D, it does in ND?
+        a = convert(Array, reshape(1:27, (3, 3, 3)))
+        b = ones(Int64, 2, 2, 2)
+        exp = convert(Array, reshape([1, 3, 5, 3,
+                                      5, 12, 16, 9,
+                                      11, 24, 28, 15,
+                                      7, 15, 17, 9,
+                                      
+                                      11, 24, 28, 15,
+                                      28, 60, 68, 36,
+                                      40, 84, 92, 48,
+                                      23, 48, 52, 27,
+                                      29, 60, 64, 33,
+                       
+                                      64, 132, 140, 72,
+                                      76, 156, 164, 84,
+                                      41, 84, 88, 45,
+                                      19, 39, 41, 21,
+                                      
+                                      41, 84, 88, 45,
+                                      47, 96,  100, 51,
+                                      25, 51, 53, 27], (4, 4, 4)))
+        @test conv(a, b) == exp
+
+
+        #6D, trivial, just to see if it works
+        a = ones(2, 2, 2, 2, 2, 2)
+        b = ones(1, 1, 1, 1, 1, 1)
+        @test conv(a, b) == a
+
+        
+        
+        # a = cat([fill(n, 3, 3) for in in 1:6]..., dims=3)
+        # b = ones(2 2)
+        # expf1 = conv(a[:, :, 1], b)
+        # exp = cat([expf1 * n for n in 1:6]..., dims=3)
+        # @test conv(a, b) = exp
+
+        # a = ones(6, 6, 10)
+        # b = cat([fill(n, 2, 2) for n in 1:3], dims=3)
+        # exp = 
+    end
+end
+
 if :xcorr in names(DSP) # VERSION >= v"0.7.0-DEV.602"
     @testset "xcorr" begin
         @test xcorr([1, 2], [3, 4]) == [4, 11, 6]
@@ -58,48 +167,6 @@ if :xcorr in names(DSP) # VERSION >= v"0.7.0-DEV.602"
 end
 
 
-@testset "conv" begin
-    # Convolution
-    a = [1., 2., 1., 2.]
-    b = [1., 2., 3.]
-    @test conv(a, b) ≈ [1., 4., 8., 10., 7., 6.]
-    @test conv(complex.(a, ones(4)), complex(b)) ≈ complex.([1., 4., 8., 10., 7., 6.], [1., 3., 6., 6., 5., 3.])
-end
-
-@testset "conv2" begin
-    a =[1 2 1;
-        2 3 1;
-        1 2 1]  
-    
-    b = [3 2;
-         0 1]
-    
-    expectation = [3 8 7 2;
-                   6 14 11 3;
-                   3 10 10 3;
-                   0 1 2 1]
-    im_expectation = [3 5 5 2;
-                      3 6 6 3;
-                      3 6 6 3;
-                      0 1 1 1]
-
-    # Integers
-    # Real Integers
-    @test conv2(a, b) == expectation
-    # Complex
-    @test conv2(complex.(a, 1), complex.(b)) == complex.(expectation,
-                                                             im_expectation)
-    # Floats
-    fa = convert(Matrix{Float64}, a)
-    fb = convert(Matrix{Float64}, b)
-    fexp = convert(Matrix{Float64}, expectation)
-    im_fexp = convert(Matrix{Float64}, im_expectation)
-    # Real
-    @test conv2(fa, fb) == fexp
-    # Complex
-    @test conv2(complex.(fa, 1), complex.(fb)) == complex.(fexp,
-                                                           im_fexp)
-end
 
 @testset "deconv" begin
     # Test for issue #188: deconv mutates inputs
