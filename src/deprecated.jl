d147ea3e83fa2122fa406100b219ca260964a7be
Martin Holters <martin.holters@hsu-hh.de>
2019-04-12 09:29:00 +0200

    Remove ancient deprecation of `firfilt`

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -1,7 +1,7 @@
+# deprecations after 0.5
 Base.@deprecate_binding ZPKFilter ZeroPoleGain
 Base.@deprecate_binding TFFilter PolynomialRatio
 Base.@deprecate_binding BiquadFilter Biquad
 Base.@deprecate_binding SOSFilter SecondOrderSections
 
-@deprecate firfilt(h, x) filt(h, x)
 @deprecate conv2 conv
