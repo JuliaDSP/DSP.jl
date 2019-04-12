10a96f81d0fda5458c1bb2996f06ace54287bfa6
Martin Holters <martin.holters@hsu-hh.de>
2019-04-12 09:28:29 +0200

    Properly deprecate old filter coefficient names
    ... and stop using them in tests.

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -1,8 +1,7 @@
-const ZPKFilter = ZeroPoleGain
-const TFFilter = PolynomialRatio
-const BiquadFilter = Biquad
-const SOSFilter = SecondOrderSections
-export ZPKFilter, TFFilter, BiquadFilter, SOSFilter
+Base.@deprecate_binding ZPKFilter ZeroPoleGain
+Base.@deprecate_binding TFFilter PolynomialRatio
+Base.@deprecate_binding BiquadFilter Biquad
+Base.@deprecate_binding SOSFilter SecondOrderSections
 
 @deprecate firfilt(h, x) filt(h, x)
 @deprecate conv2 conv
