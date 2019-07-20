ff50cbe98b99ff02a4345d4d772db7436de8c58e
MA Laforge <ma.laforge.49@gmail.com>
2019-07-20 11:30:03 -0700

    Fix significant bugs in buildeye()

--- src/eyediag.jl
+++ src/eyediag.jl
@@ -35,7 +35,7 @@ Trace data is stored in a `DataEye` object as vectors of `(x,y)` sub-vectors.
 
 Inputs:
   - tbit: Bit period (s).  Defines "trigger point" of each bit start.
-  - teye: Eye period (s).  Defines how much of eye data (along x-axis) algorithm is to collect after "trigger point".  Typical values are `1.5*tbit` or `2.0*tbit`.
+  - teye: Eye period (s).  Defines how much of eye data (along x-axis) algorithm is to collect after "trigger point".  Typically, this is set to `2.0*tbit`.
   - tstart: Time of first "trigger point" (s).
 
 Example plotting with Plots.jl:
@@ -44,7 +44,7 @@ Example plotting with Plots.jl:
     tbit = 1e-9 #Assume data bit period is 1ns.
 
     #Build eye & use tstart to center data.
-    eye = buildeye(x, y, tbit, 1.5*tbit, tstart=0.2*tbit)
+    eye = buildeye(x, y, tbit, 2.0*tbit, tstart=0.2*tbit)
 
     plot(eye.vx, eye.vy)
 """
@@ -56,28 +56,33 @@ function buildeye(x::Vector, y::Vector, tbit::Number, teye::Number; tstart::Numb
 	while i <= length(x) && x[i] < tstart
 		i+=1
 	end
+
 	wndnum = 0
 	inext = i
-	done = i > length(x)
-	while !done
-		istart = inext
+	while true
+		if inext > length(x); break; end #Nothing else to add
 		wndstart = tstart+wndnum*tbit
 		nexteye = wndstart+tbit
 		wndend = wndstart+teye
+		istart = inext
+		i = istart
 		while i <= length(x) && x[i] < nexteye
 			i+=1
 		end
 		inext = i
-		while i <= length(x) && x[i] < wndend
+		while i <= length(x) && x[i] <= wndend
 			i+=1
 		end
 		if i > length(x)
 			i = length(x)
 		end
-		if i == istart; break; end #Nothing to add
-		push!(eye.vx, x[istart:i].-wndstart)
-		push!(eye.vy, y[istart:i])
-		if inext > length(x); break; end #Nothing else
+		if x[i] > wndend
+			i -= 1
+		end
+		if i > istart
+			push!(eye.vx, x[istart:i].-wndstart)
+			push!(eye.vy, y[istart:i])
+		end
 		wndnum += 1
 	end
 	return eye
