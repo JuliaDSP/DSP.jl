59241332dd430f5b0816addf9803dd3ac4bd9766
hugs <h.t.dictus@gmail.com>
2020-01-10 12:22:56 +0100

    test_warn after test for old-style seperable conv

--- test/dsp.jl
+++ test/dsp.jl
@@ -68,8 +68,8 @@ end
         a = [1, 2]
         b = [3, 4]
         c = [1 2; 3 4]
-        @test_warn conv(a, b, c)
         @test conv(a, b, c) == conv(conv(a, b), c)
+        @test_warn "seperable" conv(a, b, c)
     end
 
     @testset "conv-2D" begin
