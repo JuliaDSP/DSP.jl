f5e871f92848f3da92e99c6f10b35bd90e3f089c
Anshul Singhvi <asinghvi17@simons-rock.edu>
2020-03-27 14:36:29 +0530

    Fix the footnote in `iirnotch`
    There can be no newlines in the footnote definition, unfortunately...

--- src/Filters/design.jl
+++ src/Filters/design.jl
@@ -472,9 +472,7 @@ Second-order digital IIR notch filter [^Orfandis] at frequency `Wn` with
 bandwidth `bandwidth`. If `fs` is not specified, `Wn` is
 interpreted as a normalized frequency in half-cycles/sample.
 
-[^Orfandis]:
-Orfanidis, S. J. (1996). Introduction to signal processing.
-Englewood Cliffs, N.J: Prentice Hall, p. 370.
+[^Orfandis]: Orfanidis, S. J. (1996). Introduction to signal processing. Englewood Cliffs, N.J: Prentice Hall, p. 370.
 """
 function iirnotch(w::Real, bandwidth::Real; fs=2)
     w = normalize_freq(w, fs)
