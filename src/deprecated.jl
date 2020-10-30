d7c67e36966374c6b450807fc22ff87121bf0dd7
Martin Holters <martin.holters@hsu-hh.de>
2020-10-30 11:29:10 +0100

    Remove deprecation of `conv2`

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -3,5 +3,3 @@ Base.@deprecate_binding ZPKFilter ZeroPoleGain
 Base.@deprecate_binding TFFilter PolynomialRatio
 Base.@deprecate_binding BiquadFilter Biquad
 Base.@deprecate_binding SOSFilter SecondOrderSections
-
-@deprecate conv2 conv
