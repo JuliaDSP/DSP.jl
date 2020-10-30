dc312f78557199bf435eacb0ea510d4accb91185
Martin Holters <martin.holters@hsu-hh.de>
2020-10-30 11:32:51 +0100

    Remove deprecations of old filter coefficent names
    Namely `ZPKFilter`, `TFFilter`, `BiquadFilter`, and ` SOSFilter`.

--- src/deprecated.jl
+++
@@ -1,5 +0,0 @@
-# deprecations after 0.5
-Base.@deprecate_binding ZPKFilter ZeroPoleGain
-Base.@deprecate_binding TFFilter PolynomialRatio
-Base.@deprecate_binding BiquadFilter Biquad
-Base.@deprecate_binding SOSFilter SecondOrderSections
