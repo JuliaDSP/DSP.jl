0557ddc9946d7a13a100325ff5e8b8087f8574db
Roger Herikstad <roger.herikstad@gmail.com>
2020-11-24 20:28:05 +0800

    Small fix in docstring for `resample` (#391)

--- src/Filters/stream_filt.jl
+++ src/Filters/stream_filt.jl
@@ -703,7 +703,7 @@ is not provided, the taps will be computed using a Kaiser window.
 
 Internally, `resample` uses a polyphase `FIRFilter` object,
 but performs additional operations to make resampling a signal easier.
-It compensates for for the `FIRFilter`'s delay (ramp-up), and appends
+It compensates for the `FIRFilter`'s delay (ramp-up), and appends
 zeros to `x`. The result is that when the input and output signals
 are plotted on top of each other, they correlate very well, but one
 signal will have more samples that the other.
