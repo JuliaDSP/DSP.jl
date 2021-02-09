4551f2ef89ecec3fda24373fa9410e0e53a2420c
ericphanson <5846501+ericphanson@users.noreply.github.com>
2021-02-09 11:46:50 +0000

    fix size of `tmp`, rename fields, return spectrogram object

--- src/multitaper_spectrogram.jl
+++ src/multitaper_spectrogram.jl
@@ -2,7 +2,7 @@ struct MultitaperedSpectrogramConfig{T, P, F}
     n_samples::Int
     sample_rate::Int
     samples_per_window::Int
-    samples_per_overlap::Int
+    n_overlap_samples::Int
     n_for_fft::Int
     n_time_points::Int
     n_freq_bins::Int
@@ -12,42 +12,39 @@ struct MultitaperedSpectrogramConfig{T, P, F}
     input_tmp::Vector{T}
     tmp::Vector{Complex{T}}
     plan::P
-    freqs::F
+    freq::F
     time::FloatRange{Float64}
     function MultitaperedSpectrogramConfig{T}(; n_samples, sample_rate, samples_per_window,
-                               samples_per_overlap,
+                               n_overlap_samples,
+                               n_for_fft = nextpow(2, samples_per_window)
                                time_bandwidth_product=4,
                                ) where T
 
-        window_duration = samples_per_window / sample_rate
-        overlap_duration = samples_per_overlap / sample_rate
-
-        # NOTE: Using powers of 2 only for now.
-        n_for_fft = nextpow(2, samples_per_window)
-
-                # Dimensions of the spectrogram power matrix
+        # Dimensions of the spectrogram power matrix
         n_time_points = div(n_samples - samples_per_window,
-                            samples_per_window - samples_per_overlap) + 1
+                            samples_per_window - n_overlap_samples) + 1
 
-        freqs = rfftfreq(n_for_fft, sample_rate)
-        n_freq_bins = length(freqs)
+        freq = rfftfreq(n_for_fft, sample_rate)
+        n_freq_bins = length(freq)
 
         n_tapers = 2 * time_bandwidth_product - 1
         
         input_tmp = zeros(T, n_for_fft)
-        tmp = zeros(Complex{T}, n_for_fft)
+        tmp = zeros(Complex{T}, n_freq_bins)
         plan = plan_rfft(input_tmp)
         dpss_window = dpss(samples_per_window, time_bandwidth_product, n_tapers)
+
+        window_duration = samples_per_window / sample_rate
+        overlap_duration = n_overlap_samples / sample_rate
 
         hop = window_duration - overlap_duration
         time = range(window_duration/2, step=hop, length = n_time_points)
 
-        return new{T, typeof(plan), typeof(freqs)}(n_samples, sample_rate, samples_per_window, samples_per_overlap,
+        return new{T, typeof(plan), typeof(freq)}(n_samples, sample_rate, samples_per_window, n_overlap_samples,
                    n_for_fft, n_time_points, n_freq_bins, time_bandwidth_product, n_tapers,
-                   dpss_window, input_tmp, tmp, plan, freqs, time)
+                   dpss_window, input_tmp, tmp, plan, freq, time)
     end
 end
-
 
 function multitapered_spectrogram!(destination::AbstractMatrix{T}, signal::AbstractVector{T},
                                    config::MultitaperedSpectrogramConfig) where {T <: Real}
@@ -59,15 +56,13 @@ function multitapered_spectrogram!(destination::AbstractMatrix{T}, signal::Abstr
     if length(signal) != expected_signal_length
         throw(ArgumentError("length(signal) == $(length(signal)) != $expected_signal_length == config.n_samples"))
     end
-    samples_per_window = config.samples_per_window
-    samples_per_overlap = config.samples_per_overlap
 
-    subepochs = arraysplit(signal, samples_per_window, samples_per_overlap)
+    subepochs = arraysplit(signal, config.samples_per_window, config.n_overlap_samples)
     for (time_index, subepoch) in enumerate(subepochs)
         power_mt_pgram!(view(destination, time_index, :), subepoch, config)
     end
 
-    return Spectrogram(destination, config.freqs, config.time)
+    return Spectrogram(destination, config.freq, config.time)
 end
 
 function multitapered_spectrogram(signal::AbstractVector{T},
@@ -78,11 +73,10 @@ function multitapered_spectrogram(signal::AbstractVector{T},
         n_samples=length(signal),
         sample_rate=fs,
         samples_per_window = n,
-        samples_per_overlap = n_overlap)
+        n_overlap_samples = n_overlap)
 
     X = Array{T,2}(undef, config.n_time_points, config.n_freq_bins)
-    multitapered_spectrogram!(X, signal, config)
-    return X
+    return multitapered_spectrogram!(X, signal, config)
 end
 
 # Simplified, in-place version of
