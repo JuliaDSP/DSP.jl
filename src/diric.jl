e6579a87e3c10b20e01991224b6a1e40df3bbfdb
Jeff Fessler <fessler@umich.edu>
2021-07-26 22:37:12 -0400

    even n with doubled precision

--- src/diric.jl
+++ src/diric.jl
@@ -15,7 +15,7 @@ of a `n`-point moving average filter.
 
 When `n` is odd or even, the function is 2π or 4π periodic, respectively.
 The formula for general `n` is
-`diric(Ω,n) = ``e^{i (n-1) Ω/2}/n \\sum_{k=0}^{n-1} e^{i k Ω}``.
+`diric(Ω,n) = ``e^{-i (n-1) Ω/2}/n \\sum_{k=0}^{n-1} e^{i k Ω}``.
 
 When `Ω` is an `AbstractFloat` (e.g., `Float32` or `BigFloat`),
 the return type matches that of `Ω`.  Otherwise the return type is `Float64`.
@@ -40,24 +40,39 @@ julia> diric(0, 4)
 """
 function diric(Ω::T, n::Int) where T <: AbstractFloat
     n > 0 || throw(ArgumentError("n=$n is non-positive"))
+    sign = 1
     if isodd(n)
         Ω = rem2pi(Ω, RoundNearest) # [-π,π)
     else
-        Ω = 2 * rem2pi(Ω/2, RoundNearest) # [-2π,2π)
+        Ω = BigFloat(Ω ; precision = 2*precision(T))
+        Ω = 2 * rem2pi(abs(Ω)/2, RoundNearest) # [0,2π)
+        if Ω > π
+            sign = -1
+            Ω -= 2π # (-π,π]
+        end
     end
 
     denom = sin(Ω / 2)
-    if abs(denom) ≤ eps(T) # denom ≈ 0 ?
+    atol = eps(T)
+    if abs(denom) ≤ atol # denom ≈ 0 ?
+#=
         if iseven(n) && abs(Ω) ≈ 2T(π)
             # 2nd-order Taylor expansion near ±2π for even n
             return abs2(abs(Ω)-2T(π)) * (n*n - 1) / 24 - one(T)
         end
+=#
     #   return one(T) - abs2(Ω) * (n*n - 1) / 24 # 2nd-order Taylor near 0
-        return one(T)
+        return sign * one(T)
     end
 
-    return sin(Ω * n/2) / (n * denom) # typical case
+    return T(sign * sin(Ω * n/2) / (n * denom)) # typical case
 end
 
-# handle non AbstractFloat types (e.g., Int, Irrational)
-diric(Ω::Real, n::Int; kwargs...) = diric(Float64(Ω), n::Int; kwargs...)
+# handle non AbstractFloat types (e.g., Int, Rational)
+diric(Ω::Real, n::Int) = diric(Float64(Ω), n::Int)
+
+# handle π exactly
+function diric(::Irrational{:π}, n::Int)
+    n > 0 || throw(ArgumentError("n=$n is non-positive"))
+    return 1 // n
+end
