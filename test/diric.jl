20d0d42562b0c67d92e27682588493d81b7fbb8c
Jeff Fessler <JeffFessler@users.noreply.github.com>
2021-07-28 06:55:01 -0400

    Update test/diric.jl
    Co-authored-by: Martin Holters <martin.holters@hsu-hh.de>

--- test/diric.jl
+++ test/diric.jl
@@ -2,13 +2,13 @@
 
 @testset "diric" begin
     @test_throws ArgumentError diric(0, -2)
-    @test @inferred diric(0, 4) ≈ 1
-    @test @inferred diric(0 // 1, 5) == 1
-    @test @inferred diric(4π, 4) ≈ 1
-    @test @inferred diric(2π, 4) ≈ -1
-    @test @inferred diric(0, 5) ≈ 1
-    @test @inferred diric(2π, 5) ≈ 1
-    @test @inferred diric(π, 5) == 1 // 5
+    @test @inferred(diric(0, 4)) ≈ 1
+    @test @inferred(diric(0 // 1, 5)) == 1
+    @test @inferred(diric(4π, 4)) ≈ 1
+    @test @inferred(diric(2π, 4)) ≈ -1
+    @test @inferred(diric(0, 5)) ≈ 1
+    @test @inferred(diric(2π, 5)) ≈ 1
+    @test @inferred(diric(π, 5)) == 1 // 5
 
     # check type inference
     @inferred diric(2, 4)
