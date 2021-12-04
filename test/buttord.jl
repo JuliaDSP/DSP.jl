9c5598e3b5681b5023e88e93ed53bee3e2ddb233
Jordan S <46058723+jsmithnh09@users.noreply.github.com>
2021-12-04 17:13:12 -0500

    Adding test cases.
    Adding test cases based on the results provided by MATLAB.

--- test/buttord.jl
+++ test/buttord.jl
@@ -11,9 +11,19 @@ using DSP, Test
     # [n, Wn] = buttord(Wp, Ws, 3, 60)
     #
 
-    (n, Wn) = buttord(40/500, 150/500, 3, 60, Lowpass)
+    (n, Wn) = buttord(40/500, 150/500, 3, 60)
     @test n == 5
-    @test isapprox(Wn, 0.0810, rtol=1e-3)
+    @test isapprox(Wn, 0.081038494957764, rtol=1e-12)
+
+    #
+    # Highpass filter example
+    # Wp = 600/2000; Ws = 1200/2000;
+    # Rs = 3; Rp = 60;
+
+    (nhpf, Wnhpf) = buttord(600/2000, 1200/2000, 3, 60)
+    @test nhpf == 7
+    @test isapprox(Wnhpf, 0.301783479785, rtol=1e-12)
+
 
     #
     # https://www.mathworks.com/help/signal/ref/buttord.html#d123e9937
@@ -26,8 +36,23 @@ using DSP, Test
     # [n, Wn] = buttord(Wp, Ws, Rp, Rs)
     #
 
-    (nbp, Wnbp) = buttord([100/500, 200/500], [50/500, 250/500], 3, 40, Bandpass)
+    (nbp, Wnbp) = buttord([100/500, 200/500], [50/500, 250/500], 3, 40)
     @test nbp == 8
-    @test isapprox(Wnbp[1], 0.1951, rtol=1e-3)
-    @test isapprox(Wnbp[2], 0.4080, rtol=1e-3)
+    @test isapprox(Wnbp[1], 0.195101359239, rtol=1e-12)
+    @test isapprox(Wnbp[2], 0.408043633382, rtol=1e-12)
+
+    #
+    # Bandstop Example, (44.1 kHz Nyquist)
+    # Wp = [3200 7800]/22050
+    # Ws = [4800 5600]/22050
+    # Rp = 2
+    # Rs = 60
+
+    # this test may be more sensitive...MATLAB's implementation of bounded minimization
+    # will yield different results in comparison to Optim.jl.
+    (nbs, Wnbs) = buttord([3200/22050, 7800/22050], [4800/22050, 5600/22050], 2, 60)
+    @test nbs == 5
+    @test isapprox(Wnbs[1], 0.172660908966, rtol=1e-12)
+    @test isapprox(Wnbs[2], 0.314956388749, rtol=1e-12)
+
 end
