91d06fb49b14e327c2b6ad52f3e843ffc0d09d8d
Jordan Smith <jsmithnh09@gmail.com>
2022-11-13 11:44:39 -0500

    Indicating that the order is an initial estimate.

--- src/Filters/filt_order.jl
+++ src/Filters/filt_order.jl
@@ -465,9 +465,12 @@ end
 Order estimation for lowpass digital filter cases based
 on the equations and coefficients in [^Rabiner]. The original
 equation returned the minimum filterlength, whereas this implementation 
-returns the order (N=L-1). Results are valid within 1.3% error. `Wp` and 
-`Ws` are the normalized passband and stopband frequencies, with `Rp` indicating 
-the passband ripple and `Rs` is the stopband attenuation (linear.) 
+returns the order (N=L-1). `Wp` and  `Ws` are the normalized passband and 
+stopband frequencies, with `Rp` indicating the passband ripple and `Rs` is the 
+stopband attenuation (linear.) 
+
+NOTE: The value of N is an initial approximation. If under-estimated, the order
+should be increased until the design specifications are met.
 
 [^Rabiner]: Herrmann, O., Lawrence R. Rabiner, and D. S. K. Chan. "Practical 
 design rules for optimum finite impulse response lowpass digital filters." Bell System 
