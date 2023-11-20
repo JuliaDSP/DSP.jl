d246969978df66dc37f1b9eaf8dde451e88bb2f0
wheeheee <104880306+wheeheee@users.noreply.github.com>
2023-11-20 15:27:33 +0800

    reflection_coeffs, `levinson` and `arburg`
    - taking reference from PR 171
    - separate `levinson` and `arburg` from `lpc` and export
    - reflection coefficients
    - `p::Integer`

--- src/lpc.jl
+++ src/lpc.jl
@@ -5,14 +5,14 @@ using ..DSP: xcorr
 
 using LinearAlgebra: dot
 
-export lpc, LPCBurg, LPCLevinson
+export lpc, arburg, levinson, LPCBurg, LPCLevinson
 
 # Dispatch types for lpc()
 struct LPCBurg end
 struct LPCLevinson end
 
 """
-    lpc(x::AbstractVector, p::Int, [LPCBurg()])
+    lpc(x::AbstractVector, p::Integer, [LPCBurg()])
 
 Given input signal `x` and prediction order `p`, returns IIR coefficients `a`
 and average reconstruction error `prediction_err`. Note that this method does
@@ -25,8 +25,14 @@ either `LPCBurg` or `LPCLevinson`.
 """
 function lpc end
 
+function lpc(x::AbstractVector{<:Number}, p::Integer, ::LPCBurg)
+    a, prediction_err = arburg(x, p)
+    popfirst!(a)
+    a, prediction_err
+end
+
 """
-    lpc(x::AbstractVector, p::Int, LPCBurg())
+    arburg(x::AbstractVector, p::Integer)
 
 LPC (Linear-Predictive-Code) estimation, using the Burg method. This function
 implements the mathematics published in [1].
@@ -35,7 +41,7 @@ implements the mathematics published in [1].
 (DAFX 2003 article, Lagrange et al)
 http://www.sylvain-marchand.info/Publications/dafx03.pdf
 """
-function lpc(x::AbstractVector{<:Number}, p::Int, ::LPCBurg)
+function arburg(x::AbstractVector{<:Number}, p::Integer)
     # Initialize prediction error with the variance of the signal
     prediction_err = sum(abs2, x) / length(x)
     T = typeof(prediction_err)
@@ -44,6 +50,7 @@ function lpc(x::AbstractVector{<:Number}, p::Int, ::LPCBurg)
     eb = copy(ef)                       # backwards error
     a = zeros(T, p + 1); a[1] = 1       # prediction coefficients
     rev_buf = similar(a, p)             # buffer to store a in reverse
+    reflection_coeffs = zeros(T, p)     # reflection coefficients
 
     @views for m in 1:p
         pop!(ef)
@@ -57,14 +64,20 @@ function lpc(x::AbstractVector{<:Number}, p::Int, ::LPCBurg)
         rev_buf[1:m] = a[m:-1:1]
         @. a[2:m+1] += k * rev_buf[1:m]
         prediction_err *= (1 - k^2)
+        reflection_coeffs[m] = k
     end
 
-    popfirst!(a)
-    return a, prediction_err
+    return a, prediction_err, reflection_coeffs
+end
+
+function lpc(x::AbstractVector{<:Number}, p::Integer, ::LPCLevinson)
+    R_xx = xcorr(x,x)[length(x):end]
+    a, prediction_err = levinson(R_xx, p)
+    a, prediction_err
 end
 
 """
-    lpc(x::AbstractVector, p::Int, LPCLevinson())
+    levinson(x::AbstractVector, p::Integer, LPCLevinson())
 
 LPC (Linear-Predictive-Code) estimation, using the Levinson method. This
 function implements the mathematics described in [1].
@@ -73,25 +86,27 @@ function implements the mathematics described in [1].
 (N. Levinson, Studies in Applied Mathematics 25(1946), 261-278,
 https://doi.org/10.1002/sapm1946251261)
 """
-function lpc(x::AbstractVector{<:Number}, p::Int, ::LPCLevinson)
-    R_xx = xcorr(x,x)[length(x):end]
-    a = zeros(p)
+function levinson(R_xx::AbstractVector{<:Number}, p::Integer)
+    # for m = 1
+    a_1 = -R_xx[2] / R_xx[1]
+    prediction_err = R_xx[1] * (one(a_1) - a_1^2)
+    T = typeof(prediction_err)
+
+    a = zeros(T, p)
+    reflection_coeffs = zeros(T, p)
+    a[1] = reflection_coeffs[1] = a_1
     rev_buf = similar(a, p - 1)     # buffer to store a in reverse
 
-    # for m = 1
-    a[1] = -R_xx[2] / R_xx[1]
-    prediction_err = R_xx[1] * (1 - a[1]^2)
-
-    # for m = 2,3,4,..p
     @views for m = 2:p
-        a[m] = (-(R_xx[m+1] + dot(a[1:m-1], R_xx[m:-1:2])) / prediction_err)
+        k = (-(R_xx[m+1] + dot(a[1:m-1], R_xx[m:-1:2])) / prediction_err)
         rev_buf[1:m-1] = a[m-1:-1:1]
-        @. a[1:m-1] += a[m] * rev_buf[1:m-1]
-        prediction_err *= (1 - a[m]^2)
+        @. a[1:m-1] += k * rev_buf[1:m-1]
+        a[m] = reflection_coeffs[m] = k
+        prediction_err *= (1 - k^2)
     end
 
-    # Return autocorrelation coefficients and error estimate
-    a, prediction_err
+    # Return autocorrelation coefficients, error estimate, and reflection coefficients
+    a, prediction_err, reflection_coeffs
 end
 
 
