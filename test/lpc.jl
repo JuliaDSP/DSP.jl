6eadb6c1adcd4b30bffaecb6796af2d7814e0ce8
wheeheee <104880306+wheeheee@users.noreply.github.com>
2023-11-23 16:57:37 +0800

    revert to test `all(<(0.01), ...)`

--- test/lpc.jl
+++ test/lpc.jl
@@ -1,20 +1,12 @@
 # Filter some noise, try to learn the coefficients
-@testset "$method, Float64" for method in (LPCBurg(), LPCLevinson())
-    coeffs = [1, .5, .3, .2]
-    x = filt(1, coeffs, randn(50000))
+@testset "$method" for method in (LPCBurg(), LPCLevinson())
+    @testset "$T" for T in (Float64, ComplexF64)
+        coeffs = T <: Complex ? [1, 0.5 + 0.2im, 0.3 - 0.5im, 0.2] : [1, .5, .3, .2]
+        x = filt(1, coeffs, randn(T, 50000))
 
         # Analyze the filtered noise, attempt to reconstruct the coefficients above
-    ar, e = lpc(x[1000:end], length(coeffs)-1, method)
+        ar, e = lpc(x[1000:end], length(coeffs)-1, method)
 
-    @test isapprox(ar, coeffs[2:end]; atol=0.01)
-end
-
-@testset "$method, ComplexF64" for method in (LPCBurg(), LPCLevinson())
-    coeffs = [1, 0.5 + 0.2im, 0.3 - 0.5im, 0.2]
-    x = filt(1, coeffs, randn(ComplexF64, 50000))
-
-    # Analyze the filtered noise, attempt to reconstruct the coefficients above
-    ar, e = lpc(x[1000:end], length(coeffs) - 1, method)
-
-    @test isapprox(ar, coeffs[2:end]; atol=0.01)
+        @test all(<(0.01), abs.(ar - coeffs[2:end]))
+    end
 end
