9f2a1d5ae1d6dd6531d8ce55c37ff5d274dc0d78
Miguel Bazdresch <miguelb@2pif.info>
2024-01-25 14:57:58 -0500

    Add tests for `jacobsen` estimator.

--- test/estimation.jl
+++ test/estimation.jl
@@ -21,20 +21,43 @@ using DSP, Test
 end
 
 @testset "jacobsen" begin
-    # real input
+    # real input: test at two arbitrary frequencies
     fs = 100
     t = range(0, 5, step = 1/fs)
     fr = 28.3
     sr = cos.(2π*fr*t .+ π/4.2)
     f_est_real = jacobsen(sr, fs)
-    @test isapprox(f_est_real, fr, atol = 0.5)
-    @test jacobsen(zeros(10)) == 0.1
-    # complex input
+    @test isapprox(f_est_real, fr, atol = 1e-5)
+    fr = 12.45
+    sr = sin.(2π*fr*t .+ 3π/2.2)
+    f_est_real = jacobsen(sr, fs)
+    @test isapprox(f_est_real, fr, atol = 1e-5)
+    # test at higher extreme of DFT
+    fr = 49.9002
+    sr = cos.(2π*fr*t)
+    f_est_real = jacobsen(sr, fs)
+    @test isapprox(f_est_real, fr, atol = 1e-5)
+    # test at lower extreme of DFT
+    @test jacobsen(zeros(10)) == 0.0
+    # complex input: test at two arbitrary frequencies
     fc = -40.3
     sc = cis.(2π*fc*t .+ π/1.4)
     f_est_complex = jacobsen(sc, fs)
+    @test isapprox(f_est_complex, fc, atol = 1e-5)
+    fc = 14.3
+    sc = cis.(2π*fc*t .+ π/3)
+    f_est_complex = jacobsen(sc, fs)
     @test isapprox(f_est_complex, fc, atol = 1e-2)
-    @test jacobsen(zeros(ComplexF64, 10)) == -0.5
+    # test at higher extreme of DFT
+    fr = 49.90019
+    sr = cis.(2π*fr*t)
+    f_est_real = jacobsen(sr, fs)
+    @test isapprox(f_est_real, fr, atol = 1e-5)
+    # test at lower extreme of DFT
+    fr = -49.90019
+    sr = cis.(2π*fr*t)
+    f_est_real = jacobsen(sr, fs)
+    @test isapprox(f_est_real, fr, atol = 1e-5)
 end
 
 @testset "quinn" begin
