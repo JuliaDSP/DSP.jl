85b8b57198c3a1cc1f00102ef01446a7fb0066f8
wheeheee <104880306+wheeheee@users.noreply.github.com>
2024-02-06 12:02:07 +0800

    add tests
    - error should be close to std of rand function which is 1
    - test `dotu` with different eltypes

--- test/lpc.jl
+++ test/lpc.jl
@@ -8,5 +8,14 @@
         ar, e = lpc(x[1000:end], length(coeffs)-1, method)
 
         @test all(<(0.01), abs.(ar .- coeffs[2:end]))
+        @test isapprox(e, 1; rtol=0.01)
     end
+end
+
+# test dotu, levinson with Int coefficients
+@test isapprox(levinson(1:10, 3)[1], -[1.25, 0, 0.25])
+
+# test that lpc defaults to Burg
+@test let v = rand(1000)
+    lpc(v, 20) == lpc(v, 20, LPCBurg())
 end
