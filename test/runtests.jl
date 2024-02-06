956397284863ca1fb50336e170beebfa459963ea
wheeheee <104880306+wheeheee@users.noreply.github.com>
2024-02-06 10:24:57 +0800

    wrap test includes in testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,14 +3,26 @@ using DSP, FFTW, Test
 using DSP: allocate_output
 using Random: seed!
 
-testfiles = ["dsp.jl", "util.jl", "windows.jl", "filter_conversion.jl",
-    "diric.jl",
-    "filter_design.jl", "filter_response.jl", "filt.jl", "filt_order.jl", "filt_stream.jl",
-    "periodograms.jl", "multitaper.jl", "resample.jl", "lpc.jl", "estimation.jl", "unwrap.jl",
-    "remez_fir.jl" ]
+module_tests = [
+    :DSPBase => ["dsp.jl", "diric.jl"],
+    :Utils => ["util.jl"],
+    :Windows => ["windows.jl"],
+    :Filters => [
+        "filter_conversion.jl", "filter_design.jl", "filter_response.jl", "filt.jl",
+        "filt_order.jl", "filt_stream.jl", "resample.jl", "remez_fir.jl"
+    ],
+    :Periodograms => ["periodograms.jl", "multitaper.jl"],
+    :LPC => ["lpc.jl"],
+    :Estimation => ["estimation.jl"],
+    :Unwrap => ["unwrap.jl"]
+]
 
 seed!(1776)
 
-for testfile in testfiles
-    eval(:(@testset $testfile begin include($testfile) end))
+@testset verbose=true "DSP.jl" begin
+    @testset verbose=true "$(first(module_test))" for module_test in module_tests
+        @testset "$testfile" for testfile in last(module_test)
+            include(testfile)
+        end
+    end
 end
