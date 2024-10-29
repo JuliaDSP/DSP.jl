7c1a7cf774acb257e67d38fe89cdaa2040b844d7
wheeheee <104880306+wheeheee@users.noreply.github.com>
2024-10-29 18:00:01 +0800

    try changing tests

--- test/filt.jl
+++ test/filt.jl
@@ -271,8 +271,8 @@ end
 # fir_filtfilt
 @testset "fir_filtfilt" begin
     b = randn(10)
-    for x in (randn(100), randn(100, 2), randn(10))   # also tests issue #537
-        @test filtfilt(b, x) ≈ DSP.Filters.iir_filtfilt(b, [1.0], x)
+    for x in (randn(100), randn(100, 2), randn(10), [1:10;])   # also tests issue #537
+        @test filtfilt(b, x) ≈ DSP.Filters.iir_filtfilt(b, [1], x)
         @test filtfilt(b, [2.0], x) ≈ DSP.Filters.iir_filtfilt(b, [2], x)
     end
 end
