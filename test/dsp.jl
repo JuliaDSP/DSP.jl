cc4cb36ab23ef95426a02d33583f883d7ab07831
wheeheee <104880306+wheeheee@users.noreply.github.com>
2024-12-15 00:27:30 +0800

    start moving to convolutions.jl
    move nextfastfft to convolutions.jl

--- test/dsp.jl
+++ test/dsp.jl
@@ -2,9 +2,8 @@
 # TODO: parameterize conv tests
 using Test, OffsetArrays
 using DSP: filt, filt!, deconv, conv, xcorr,
-           optimalfftfiltlength, unsafe_conv_kern_os!, _conv_kern_fft!
            nextfastfft
-
+using DSP.Convolutions: optimalfftfiltlength, unsafe_conv_kern_os!, _conv_kern_fft!
 
 
 @testset "filt" begin
