5b212659b8314903a1ca596a2b2c2ff2fd8068a1
wheeheee <104880306+wheeheee@users.noreply.github.com>
2025-03-05 14:14:37 +0800

    using, comma

--- test/dsp.jl
+++ test/dsp.jl
@@ -1,8 +1,8 @@
 # This file was formerly a part of Julia. License is MIT: https://julialang.org/license
 # TODO: parameterize conv tests
 using Test, OffsetArrays
-using DSP: filt, filt!, deconv, conv, xcorr,
-           optimalfftfiltlength, unsafe_conv_kern_os!, _conv_kern_fft!
+using DSP: filt, filt!, deconv, conv, conv!, xcorr,
+           optimalfftfiltlength, unsafe_conv_kern_os!, _conv_kern_fft!,
            nextfastfft
 
 
